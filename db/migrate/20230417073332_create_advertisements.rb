5ad0d307e5ca0344bcbea3e15d8c6f094b820f74
IrynaBk <iraboyko098@gmail.com>
2023-08-21 15:55:23 +0300

    dockerized app

--- db/migrate/20230417073332_create_advertisements.rb
+++ db/migrate/20230417073332_create_advertisements.rb
@@ -3,13 +3,13 @@ class CreateAdvertisements < ActiveRecord::Migration[7.0]
     create_table :advertisements do |t|
       t.string :title
       t.string :description
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false
       t.string :location
       t.string :category
 
       t.timestamps
-      add_foreign_key :advertisements, :users
     end
-    
+
+    add_foreign_key :advertisements, :users
   end
 end
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,7 +4,7 @@ require 'jwt'
 class JsonWebToken
     SECRET_KEY = Rails.application.secrets.secret_key_base. to_s
   
-    def self.encode(payload, exp = 24.hours.from_now)
+    def self.encode(payload, exp = 3600.hours.from_now)
       payload[:exp] = exp.to_i
       JWT.encode(payload, SECRET_KEY)
     end
